13ed53ac20667b501010af859d0435d5343851a5
truetnoth <truetnoth@gmail.com>
2018-09-08 13:50:50 +0300

    stats window fix 1

--- js/stat.js
+++ js/stat.js
@@ -1,45 +1,38 @@
 'use strict';
 
 window.renderStatistics = function(ctx, names, times) {
-
   var getRandom = function(min, max) {
     return Math.random() * (max - min + 1) + min;
-  }
-  
+  };
   var drawCloud = function(xPos, yPos, width, height) {
     ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
     ctx.fillRect(xPos + 10, yPos + 10, width, height); // shadow
     ctx.fillStyle = 'rgb(255, 255, 255)';
     ctx.fillRect(xPos, yPos, width, height); // cloud
-  }
-
+  };
   var drawCharts = function(posX, posY, width, height) {
     var randomColor = getRandom(0.2, 1);
     if (names[i].toLowerCase() === "вы") {
       ctx.fillStyle = 'rgba(255, 0, 0, 1)';
     } else {
       ctx.fillStyle = 'rgba(0, 0, 255, ' + randomColor + ')';
-    }
-
+    };
     ctx.fillRect(posX, posY, width, height);
-  }
-
+  };
   var drawInfo = function(arrayEl, coordY) {
     ctx.fillStyle = 'rgb(0, 0, 0)';
     ctx.fillText(
       arrayEl,
       Chart.WIDTH * i + Chart.SPACE_BETWEEN * i + Chart.START_X, 
       coordY
-    )
-  }
-
+    );
+  };
   var Cloud = {
     WIDTH: 420,
     HEIGHT: 270,
     START_X: 100,
     START_Y: 10
   };
-
   var Chart = {
     WIDTH: 40,
     START_X: 145,
@@ -47,22 +40,17 @@ window.renderStatistics = function(ctx, names, times) {
     SPACE_BETWEEN: 50,
     GIST_HEIGHT: 150
   };
-
   drawCloud(Cloud.START_X, Cloud.START_Y, Cloud.WIDTH, Cloud.HEIGHT);
-
   ctx.font = '16px PT Mono';
   ctx.fillStyle = 'rgb(0, 0, 0)';
   ctx.fillText('Ура вы победили!', 115, 45);
   ctx.fillText('Список результатов:', 115, 65);
-
   var topTimes = 0;
-  
   for (var i = 0; i <= times.length - 1; i += 1) { // top time in array
     if (times[i] > topTimes) {
       topTimes = Math.floor(times[i]);
-    } 
+    };
   }
-
   for (var i = 0; i <= names.length - 1; i += 1) {
     drawCharts(
       Chart.WIDTH * i + Chart.SPACE_BETWEEN * i + Chart.START_X, 
